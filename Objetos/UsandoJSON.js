00b3c96c59133414fbe1e370f8ae078accd686c4
paulosergio <paulosergiobarrosdesouza@gmail.com>
2024-08-13 14:19:29 -0300

    Código finalizado

--- Objetos/UsandoJSON.js
+++ Objetos/UsandoJSON.js
@@ -1,20 +1,26 @@
 let carrinho = {
   nome: "Paulo Sérgio",
   idade: 21,
-  produtoValor: [[
+  produtoValor: [
     ["Monitor Pichau", 999.99],
     ["Gabinete Pichau", 250],
     ["Ventoinhas", 150],
     ["Mesa", 800]
-  ]]
+  ],
 }
-  
+
 exibir(carrinho)
 
-function exibir(carrinho){
-  //console.log(`${carrinho.nome} comprou em seu aniverssário um ${carrinho.produtoValor[0][0]}`)
+function exibir(carrinho,){
+
+  let total = 0
+
+  console.log(`${carrinho.nome} em seu aniverssario comprou:`)
+  
   for(let i = 0; i < carrinho.produtoValor.length; i++){
-    console.log(`${carrinho.nome} em seu aniverssario comprou:`)
-    console.log(carrinho.produtoValor[i][0])
+    console.log(`- ${carrinho.produtoValor[i][0]} $: ${carrinho.produtoValor[i][1]}`)
+    total += carrinho.produtoValor[i][1]
   }
+
+  console.log(`Valor total dos produtos: R$ ${total}`)
 }
